c47350fbaf1113492c48ab121b23dfb8e048ba08
Nick Fairburn <110976174+Nick-F997@users.noreply.github.com>
2024-11-18 09:32:14 +0000

    Added comments to interpretation code

--- app/inc/interpreter.h
+++ app/inc/interpreter.h
@@ -1,7 +1,9 @@
 /**
  * @file interpreter.h
  * @author Nicholas Fairburn (nicholas2.fairburn@live.uwe.ac.uk)
- * @brief Header containing definitions and prototypes for basic line by line interpreter.
+ * @brief Header containing definitions and prototypes for basic line by line interpreter,.
+ * @note much of this relies on work completed by Robert Nystrom in his wonderful work Crafting Interpreters.
+ *       Much of this command line interpreter is adapted from the C-Lox interpeter, but with major modifications. 
  * @version 0.1
  * @date 2024-11-15
  * 
@@ -23,6 +25,10 @@
 #include "board-control.h"
 
 // Enum defs
+/**
+ * @brief Enum that dictates the tokens available to the user.
+ * 
+ */
 typedef enum TokenType {
     TOKEN_GPIO_INPUT,
     TOKEN_GPIO_OUTPUT,
@@ -35,11 +41,23 @@ typedef enum TokenType {
 } TokenType;
 
 // Struct definitions
+/**
+ * @brief Scanner struct. Scans inputted text and creates tokens.
+ * @param start start of current line to be analysed.
+ * @param current character from which line is being analysed.
+ * 
+ */
 typedef struct Scanner {
     const char *start;
     const char *current;
 } Scanner;
 
+/**
+ * @brief Token struct. Emitted by scanner after tokenisation.
+ * @param type type of token
+ * @param start start of the string for the token
+ * @param length length of the string.
+ */
 typedef struct Token {
     TokenType type;
     const char *start;
--- app/src/interpreter.c
+++ app/src/interpreter.c
@@ -1,29 +1,58 @@
 #include "interpreter.h"
 
-
-
+/**
+ * @brief Looks at the current character in a string.
+ * 
+ * @param scanner scanner to check next char
+ * @return char 
+ */
 static char peek(Scanner *scanner)
 {
     return *scanner->current;
 }
 
+/**
+ * @brief increments current character to be analysed and returns previous character.
+ * 
+ * @param scanner scanner object to be advanced.
+ * @return char 
+ */
 static char advance_scanner(Scanner *scanner)
 {
     scanner->current++;
     return scanner->current[-1];
 }
 
+/**
+ * @brief checks whether the character is a numerical symbol
+ * 
+ * @param c character to check
+ * @return true c is digit
+ * @return false c is not digit
+ */
 static bool isDigit(char c)
 {
     return c >= '0' && c <= '9';
 }
 
+/**
+ * @brief checks whether character is alphabetic.
+ * 
+ * @param c character to check
+ * @return true character is alphabetic
+ * @return false character is not alphabetic
+ */
 static bool isAlpha(char c) {
   return (c >= 'a' && c <= 'z') ||
          (c >= 'A' && c <= 'Z') ||
           c == '_';
 }
 
+/**
+ * @brief Jumps over whitespace. 
+ * 
+ * @param scanner scanner to jump
+ */
 static void skipWhitespace(Scanner *scanner)
 {
     while (true)
@@ -45,18 +74,37 @@ static void skipWhitespace(Scanner *scanner)
     }
 }
 
-
+/**
+ * @brief Initialises Scanner object
+ * 
+ * @param scanner object to be initialised
+ * @param source source code to initialise to.
+ */
 static void initScanner(Scanner *scanner, const char *source)
 {
     scanner->start = source;
     scanner->current = source;
 }
 
+/**
+ * @brief Checks whether scanner is at the end of the line or not.
+ * 
+ * @param scanner scanner to check
+ * @return true scanner is at end
+ * @return false scanner is not at end
+ */
 static bool is_at_end(Scanner *scanner)
 {
     return *scanner->current == '\0';
 }
 
+/**
+ * @brief Creates token from current string segment
+ * 
+ * @param scanner scanner to create token from
+ * @param type type of token
+ * @return Token the created token.
+ */
 static Token makeToken(Scanner *scanner, TokenType type)
 {
     Token token;
@@ -66,9 +114,23 @@ static Token makeToken(Scanner *scanner, TokenType type)
     return token;
 }
 
+/**
+ * @brief Checks whether the current token in scanner matches the provided "rest of string". For example, 
+ * if Scanner's current string starts with "i", and rest is given as "nput", if the rest of Scanner's string 
+ * is "input", this function returns the "input" token. If it does not match, it returns the "identifier" token. 
+ * 
+ * @param scanner the scanner to check.
+ * @param start From which character we are starting from (0 indexed)
+ * @param length the length of the remaining string.
+ * @param rest the rest of the string
+ * @param type the type to be checked against.
+ * @return TokenType 
+ */
 static TokenType checkKeyword(Scanner *scanner, int start, int length, const char *rest, TokenType type)
 {
-    if (scanner->current - scanner->start == start + length && memcmp(scanner->start + start, rest, length) == 0)
+    // Ensure that the provided "rest of string" is both the same length and matches the text.
+    if (scanner->current - scanner->start == start + length 
+            && memcmp(scanner->start + start, rest, length) == 0)
     {
         return type;
     }
@@ -76,8 +138,15 @@ static TokenType checkKeyword(Scanner *scanner, int start, int length, const cha
     return TOKEN_PORT_PIN;
 }
 
+/**
+ * @brief Checks what the current string starts with and matches that to a token.
+ * 
+ * @param scanner scanner to be checked
+ * @return TokenType the matched token
+ */
 static TokenType identiferType(Scanner *scanner)
 {
+    // Switch on first character in string.
     switch (scanner->start[0])
     {
         case 'i':
@@ -105,12 +174,25 @@ static TokenType identiferType(Scanner *scanner)
     return TOKEN_PORT_PIN;
 }
 
+/**
+ * @brief Advances through current string until it encounters something that isn't a token.
+ * Then analyses that string and returns the matching token.
+ * 
+ * @param scanner scanner to be checked
+ * @return Token created token.
+ */
 static Token identifier(Scanner *scanner)
 {
     while (isAlpha(peek(scanner)) || isDigit(peek(scanner))) advance_scanner(scanner);
     return makeToken(scanner, identiferType(scanner));
 }
 
+/**
+ * @brief Function for debugging. Returns name of token.
+ * 
+ * @param token token to get name of
+ * @return char* 
+ */
 static char *get_token_type_name(TokenType token)
 {
     switch (token)
@@ -151,24 +233,41 @@ static char *get_token_type_name(TokenType token)
     return "UNKNOWN_TOKEN";
 }
 
+/**
+ * @brief Debugging function for printing token info
+ * 
+ * @param token token to be printed.
+ */
 static void print_token(Token token)
 {
     printf("Token type: %s\r\nToken: %.*s\r\n", get_token_type_name(token.type), token.length, token.start);
 }
 
+/**
+ * @brief Interprets a given line.
+ * 
+ * @param bc the board controller struct. 
+ * @param source the line to be interpetered.
+ * @param length length of the line, unused.
+ * @return true successful interpretation
+ * @return false unsucessful interpretation.
+ */
 bool interpret(BoardController *bc, char *source, size_t length)
 {
+    // initialise scanner obj
     Scanner scanner;
     initScanner(&scanner, source);
-    int count = 0;
-    Token tokvec[20];
+    // For debugging. Will replace with an actual vector at some point.
+    int count = 0; // size of tokvec
+    Token tokvec[20]; // Collection of tokens
 
     while (true)
     {
-        skipWhitespace(&scanner);
-        scanner.start = scanner.current;
-        if (is_at_end(&scanner))
+        skipWhitespace(&scanner); // Skip over whitespace
+        scanner.start = scanner.current; // assign first character after whitespace to be the starting char.
+        if (is_at_end(&scanner)) // When finished...
         {
+            // Debugging stuff.
             tokvec[count++] = makeToken(&scanner, TOKEN_EOL);
             printf("Token count = %d\r\n", count);
             for (int i = 0; i < count; i++)
@@ -177,12 +276,14 @@ bool interpret(BoardController *bc, char *source, size_t length)
             }
             count = 0;
 
+            // Successful interpretation.
             return true;
         }
 
+        // Move to the next character.
         char c = advance_scanner(&scanner);
-        if (isAlpha(c)) tokvec[count++] = identifier(&scanner);
-        else tokvec[count++] = makeToken(&scanner, TOKEN_ERROR);
+        if (isAlpha(c)) tokvec[count++] = identifier(&scanner); // If its a letter start checking
+        else tokvec[count++] = makeToken(&scanner, TOKEN_ERROR); // Otherwise stop
     }
 }
 
